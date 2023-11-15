d4d4ca614b90f8229bdcd91789b9cbbe38f02aad
Fabian_Laptop <fb-224651@hs-weingarten.de>
2023-11-15 20:15:37 +0100

    Add factory function createPlayer and test it
    -currently only has a privat id and a getPlayerID that is returned to
     enable read access to the playerID

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const GameBoard = (function () {
   return { placeMoveOnBoard, logBoard };
 })();
 
+const createPlayer = function (playerID) {
+  const id = playerID;
+  const getPlayerID = () => id;
+  return { getPlayerID };
+};
+
 GameBoard.logBoard();
 GameBoard.placeMoveOnBoard(2, 2);
 GameBoard.logBoard();
+
+const player1 = createPlayer(1);
+console.log(player1.getPlayerID());
